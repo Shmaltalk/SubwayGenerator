edca4d8d4a498e98eb19ce9a18f35efcdf0e5f57
Talie Massachi <talie@brown.edu>
2021-02-16 14:19:16 -0500

    Town framework (#2)
    added sigs

--- generator.rkt
+++ generator.rkt
@@ -1,1 +1,28 @@
+#lang forge "curiosity_modeling" "clU0kCu1da0mc0rN@gmail.com"
 
+
+
+
+sig Stop {
+    connections: set Stop->Int
+}
+
+sig Route {
+    path : set Stop->Stop
+}
+
+pred isTown {
+    -- connected
+
+    -- undirected
+
+    -- weighted
+
+    -- irreflexive
+}
+
+
+
+
+
+run {} for 3 Stop
