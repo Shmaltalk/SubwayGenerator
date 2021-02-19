baf5ed9a1309aa1fdf73c04a27db0a0b7b0a3556
ccobb1 <carter_cobb@brown.edu>
2021-02-19 10:48:32 -0500

    added isLine and isSubwaySystem preds

--- generator.rkt
+++ generator.rkt
@@ -8,7 +8,7 @@ sig Stop {
 }
 
 sig Route {
-    path : set Stop->Stop
+    path: set Stop->Stop
 }
 
 pred isTown {
@@ -21,8 +21,26 @@ pred isTown {
     -- irreflexive
 }
 
+pred isLine[p: Stop->Stop] {
+    -- undirected (symmetric)
+    ~p in p
 
+    -- connected
+    Stop->Stop in *p
+    
+    -- non-branching (every stop is connected to at most two other
+    -- stops, the ones "before" and "after" it)
+    all s: Stop | #s.p <= 2
+    
+    -- line should have two endpoints, each of which will be connected to only
+    -- one other stop
+    -- (note: this also ensures that the line contains at least 2 stops)
+    #{s: Stop | #s.p = 1} = 2
+}
 
+pred isSubwaySystem {
+    isTown
+    all r: Route | isLine[r.path]
+}
 
-
-run {} for 3 Stop
+run {isSubwaySystem} for exactly 1 Route, 3 Stop
