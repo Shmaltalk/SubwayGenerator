d4cd916d118f34dc3a7b34f72f8b79e9e47a732a
ShmalTalk <talie_massachi@brown.edu>
2021-02-22 23:18:16 -0500

    examples cant have for x for specification

--- generator.rkt
+++ generator.rkt
@@ -104,7 +104,18 @@ pred maxDistance[maxDist: Int] {
     }
 }
 
-run {isSubwaySystem and validStopPaths and maxDistance[sing[15]]} for exactly 4 Stop, 8 Int, 20 StopPath -- 8 just to make it a round byte
+run {isSubwaySystem and validStopPaths and isTown and maxDistance[sing[1]]} for {
+    Stop = Stop0 + Stop1
+    connections = Stop0->Stop1->sing[5] + Stop1->Stop0->sing[5]
+
+    Route = Route0
+    path = Route0->Stop0->Stop1 + Route0->Stop1->Stop0
+
+    StopPath = SP0 + SP1
+    stop1 = SP0->Stop0 + SP1->Stop1
+    stop2 = SP0->Stop1 + SP1->Stop0
+    route = SP0->Stop0->Stop1 + SP1->Stop1->Stop0
+}//exactly 4 Stop, 8 Int, 20 StopPath -- 8 just to make it a round byte
 
 
 -- test town
@@ -517,13 +528,14 @@ example countDuplicateDistances2 is {validStopPaths and isTown} for {
     dist = SP0->sing[2]
 }
 
--- maxDistance
+-- test all together
+
 test expect {
-    distTooFar: {isTown and maxDistance[sing[2]]} for 10 StopPath for {
+    distTooFar: {validStopPaths and isTown and isSubwaySystem and maxDistance[sing[2]]} for 8 Int, 10 StopPath for {
         Stop = Stop0 + Stop1
         connections = Stop0->Stop1->sing[5] + Stop1->Stop0->sing[5]
     } is unsat
-    missingStopPath: {isTown and maxDistance[sing[5]]} for 10 StopPath for {
+    missingStopPath: {validStopPaths and isTown and isSubwaySystem and maxDistance[sing[5]]} for 8 Int, 10 StopPath for {
         Stop = Stop0 + Stop1
         connections = Stop0->Stop1->sing[1] + Stop1->Stop0->sing[1]
 
@@ -533,37 +545,14 @@ test expect {
     } is unsat
 
     -- one of allowable distance and one too long
-    somePathTooLong: {isTown and maxDistance[sing[2]]} for 10 StopPath for {
+    somePathTooLong: {validStopPaths and isTown and isSubwaySystem and maxDistance[sing[2]]} for 8 Int, 10 StopPath for {
         Stop = Stop0 + Stop1 + Stop2
         connections = Stop0->Stop1->sing[5] + Stop1->Stop0->sing[5] + Stop0->Stop2->sing[1] + Stop2->Stop0->sing[1]
     } is unsat
 }
 
--- maxDistance
-test expect {
-    distTooFar: {isTown and maxDistance[sing[2]]} for 10 StopPath for {
-        Stop = Stop0 -> Stop1
-        connections = Stop0->Stop1->sing[5] + Stop1->Stop0->sing[5]
-    } is unsat
-    missingStopPath: {isTown and maxDistance[sing[5]]} for 10 StopPath for {
-        Stop = Stop0 -> Stop1
-        connections = Stop0->Stop1->sing[1] + Stop1->Stop0->sing[1]
 
-        StopPath = SP0
-        stop1 = SP0->Stop0
-        stop2 = SP0->Stop1
-    } is unsat
-
-    -- one of allowable distance and one too long
-    somePathTooLong: {isTown and maxDistance[sing[2]]} for 10 StopPath for {
-        Stop = Stop0 -> Stop1 -> Stop2
-        connections = Stop0->Stop1->sing[5] + Stop1->Stop0->sing[5] + Stop0->Stop2->sing[1] + Stop2->Stop0->sing[1]
-    } is unsat
-}
-
--- test all together
-
-/*example emptyTownOK is {validStopPaths and isTown and isSubwaySystem and maxDistance[sing[10]]} for {
+example emptyTownOK is {validStopPaths and isTown and isSubwaySystem and maxDistance[sing[10]]} for {
     Stop = none
     connections = none->none->none
 
@@ -577,7 +566,7 @@ test expect {
     dist = none
 }
 
-example singleStopTown is {validStopPaths and isTown and isSubwaySystem and maxDistance[sing[10]]} for 10 StopPath for {
+example singleStopTown is {validStopPaths and isTown and isSubwaySystem and maxDistance[sing[10]]} for {
     Stop = Stop0
     connections = none->none->none
 
@@ -591,20 +580,22 @@ example singleStopTown is {validStopPaths and isTown and isSubwaySystem and maxD
     dist = none
 }
 
-example smallTown is {validStopPaths and isTown and isSubwaySystem and maxDistance[sing[10]]} for 10 StopPath for {
-    Stop = Stop0 + Stop1 + Stop2
-    connections = Stop0->Stop1->sing[1] + Stop1->Stop0->sing[1] + 
-                  Stop1->Stop2->sing[2] + Stop2->Stop1->sing[2] +
-                  Stop0->Stop2->sing[3] + Stop2->Stop0->sing[3]
+test expect {
+  smallTown: {validStopPaths and isTown and isSubwaySystem and maxDistance[sing[10]]} for 10 StopPath for {
+      Stop = Stop0 + Stop1 + Stop2
+      connections = Stop0->Stop1->sing[1] + Stop1->Stop0->sing[1] + 
+                    Stop1->Stop2->sing[2] + Stop2->Stop1->sing[2] +
+                    Stop0->Stop2->sing[3] + Stop2->Stop0->sing[3]
 
-    Route = Route0
-    path = Route0->Stop0->Stop1 + Route0->Stop1->Stop0 + Route0->Stop1->Stop2 + Route0->Stop2->Stop1
+      Route = Route0
+      path = Route0->Stop0->Stop1 + Route0->Stop1->Stop0 + Route0->Stop1->Stop2 + Route0->Stop2->Stop1
 
-    StopPath = SP0 + SP1 + SP2 + SP3 + SP4 + SP5
-    stop1 = SP0->Stop0 + SP1->Stop0 + SP2->Stop1 + SP3->Stop1 + SP4->Stop2 + SP5->Stop2
-    stop2 = SP0->Stop1 + SP1->Stop2 + SP2->Stop0 + SP3->Stop2 + SP4->Stop0 + SP5->Stop1
-    route = SP0->Stop0->Stop1 + SP1->Stop0->Stop2 + SP2->Stop1->Stop0 + SP3->Stop1->Stop2 +
-            SP4->Stop2->Stop0 + SP5->Stop2->Stop1
-    dist = SP0->sing[1] + SP1->sing[3] + SP2->sing[1] + SP3->sing[2] + SP4->sing[3] + SP5->sing[2]
+      StopPath = SP0 + SP1 + SP2 + SP3 + SP4 + SP5
+      stop1 = SP0->Stop0 + SP1->Stop0 + SP2->Stop1 + SP3->Stop1 + SP4->Stop2 + SP5->Stop2
+      stop2 = SP0->Stop1 + SP1->Stop2 + SP2->Stop0 + SP3->Stop2 + SP4->Stop0 + SP5->Stop1
+      route = SP0->Stop0->Stop1 + SP1->Stop0->Stop2 + SP2->Stop1->Stop0 + SP3->Stop1->Stop2 +
+              SP4->Stop2->Stop0 + SP5->Stop2->Stop1
+      dist = SP0->sing[1] + SP1->sing[3] + SP2->sing[1] + SP3->sing[2] + SP4->sing[3] + SP5->sing[2]
+  } is sat
 }
-*/
+
